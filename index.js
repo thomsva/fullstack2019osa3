d4f6e745980fcbf4d9f250097fa920b95a697707
Thomas Svartbäck <thomsva@gmail.com>
2019-02-04 16:40:32 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+
+
+  {
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": 1
+  },
+  {
+    "name": "Arto Järvinen",
+    "number": "040-123456",
+    "id": 3
+  },
+  {
+    "name": "Lea Kutvonen",
+    "number": "040-123456",
+    "id": 4
+  }
+
+
+]
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
