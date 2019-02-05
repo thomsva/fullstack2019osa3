be92e3b960b0d6ef1b3a4ab8356936d8d96cedcf
Thomas Svartbäck <thomsva@gmail.com>
2019-02-05 10:15:28 +0200

    3.7 valmis

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
+
+const morgan = require('morgan')
+app.use(morgan('tiny'))
 
 let persons = [
   {
